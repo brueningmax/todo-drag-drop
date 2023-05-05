4dcd6d9eacecc7a1b57a0157fcbc67917621769f
Max Brüning <mail@maxbruening.de>
2023-05-05 18:26:44 +0200

    getBoard and getUsers done

--- sequelizer.js
+++ sequelizer.js
@@ -16,55 +16,63 @@ TodoModel.associate({ ClientModel, UserModel });
 ClientModel.associate({ TodoModel });
 UserModel.associate({ TodoModel });
 
-function reorderTodos(todos) {
-    const new_list = [];
-    const used_ids = new Set();
-    while (new_list.length < todos.length) {
-      // Find a start-todo with previous_todo value of 0
-      const start_todo = todos.find(todo => todo.previous_todo === 0 && !used_ids.has(todo.id));
-      used_ids.add(start_todo.id);
-      new_list.push(start_todo);
-      let next_todo_id = start_todo.next_todo;
-      // Add successive todos until next_todo value is 0
-      while (next_todo_id !== 0) {
-        const next_todo = todos.find(todo => todo.id === next_todo_id && !used_ids.has(todo.id));
-        used_ids.add(next_todo.id);
-        new_list.push(next_todo);
-        next_todo_id = next_todo.next_todo;
-      }
+const sortTodos = (array) => {
+    const sortedArray = []
+    let firstTodo = array.find(todo => todo.previous_todo === 0)
+    sortedArray.push(firstTodo)
+
+    const todoObj = {};
+    array.forEach(todo => {
+        todoObj[todo.id] = todo;
+    });
+
+    while (sortedArray.length < array.length){
+        let next_todo_id = String(sortedArray[sortedArray.length - 1].next_todo)
+        let next_todo = todoObj[next_todo_id]
+        sortedArray.push(next_todo) 
     }
-    return new_list;
-  }
+
+    return sortedArray
+}
 
 // synchronize the models with the database
 sequelize.sync()
-    .then(() => {
-        console.log('Database synced successfully');
-        return User.findAll({
-            attributes: ['id', 'name', 'role'],
-            include: [
-                {
-                    model: TodoModel,
-                    as: 'todos',
-                    include: [{ model: ClientModel }],
-                },
-            ],
-            order: [
-                Sequelize.literal(`case when UserModel.id = 1 then 1 when UserModel.id = 2 then 3 else 2 end`),
-            ],
+const getUsers =async () => {
+    let users = await User.findAll({
+        attributes: ['id', 'name', 'role']
+    })
+    const formattedData = users.map(user => user.dataValues)
+    return formattedData;
+}
+
+const getBoard = async () => {
+    let users = await User.findAll({
+                attributes: ['id', 'name', 'role'],
+                include: [
+                    {
+                        model: TodoModel,
+                        as: 'todos',
+                        include: [{ model: ClientModel }],
+                    },
+                ],
+                order: [
+                    Sequelize.literal(`case when UserModel.id = 1 then 1 when UserModel.id = 2 then 3 else 2 end`),
+                ],
+            })
+    const formattedData = await users.map(user => {
+        const { id, name, role, todos } = user.dataValues;
+        let todosData = todos.map(todo => {
+            let client = todo.dataValues.ClientModel.dataValues
+            todo.dataValues.client = client
+            delete todo.dataValues.ClientModel
+            return todo.dataValues
         })
-    })
-    .then((users) => {
-        const formattedData = users.map(user => {
-            const { id, name, role, todos } = user.toJSON();
-            const todosData = reorderTodos(todos)
-            return {
-                user: { id, name, role },
-                todos: todosData
-            };
-        });
-        console.log(JSON.stringify(formattedData, null, 2));
-    })
-    .catch((error) => {
-        console.error('Error syncing database', error);
+        todosData = sortTodos(todosData)
+        return {
+            user: { id, name, role },
+            todos: todosData
+        };
     });
+    return formattedData
+}
+console.log(await getBoard())
